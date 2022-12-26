ad7f6329fff59c12beea870667bd46be05f7a47d
Vasif <vasif@Vasif-MacBook-Air.local>
2022-12-26 16:26:25 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const bar=document.getElementById('bar');
+const nav=document.getElementById('navbar');
+const close=document.getElementById('close');
+
+
+if(bar){
+    bar.addEventListener('click' , () => {
+        nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click' , ()=>{
+        nav.classList.remove('active');
+    })
+}
+
+var MainImg=document.getElementById("MainImg");
+var smallimg=document.getElementsByClassName("small-img");
+
+
+smallimg[0].onclick=function(){
+    MainImg.src=smallimg[0].src;
+
+}
+smallimg[1].onclick=function(){
+    MainImg.src=smallimg[1].src;
+    
+}
+smallimg[2].onclick=function(){
+    MainImg.src=smallimg[2].src;
+    
+}
+smallimg[3].onclick=function(){
+    MainImg.src=smallimg[3].src;
+    
+}
+smallimg[4].onclick=function(){
+    MainImg.src=smallimg[4].src;
+    
+}
+smallimg[5].onclick=function(){
+    MainImg.src=smallimg[5].src;
+    
+}
+smallimg[6].onclick=function(){
+    MainImg.src=smallimg[6].src;
+    
+}
+
+
+
